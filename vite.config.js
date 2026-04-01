ef109b19a72b26f90913f084b5ae41a983984517
DINKSOFT <85507916+DINKSOFT@users.noreply.github.com>
2026-04-01 14:11:15 +0000

    Change base path in Vite config
    Updated the base path for the application to match the new repository name.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/solde-tout-compte-app/', // ← IMPORTANT : Nom de votre repository GitHub
+  base: '/solde-tout-compte-bf/', // ← IMPORTANT : Nom de votre repository GitHub
   server: {
     port: 3000,
     host: true
